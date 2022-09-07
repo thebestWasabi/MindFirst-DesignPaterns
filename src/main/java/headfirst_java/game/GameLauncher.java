5becd05a6543bb76e5bd1973f86235a3a76c167c
thebestWasabi <openpw@icloud.com>
2022-09-07 19:11:37 +0500

    0.0.3c

---
+++ src/main/java/headfirst_java/game/GameLauncher.java
@@ -0,0 +1,9 @@
+package headfirst_java.game;
+
+public class GameLauncher {
+
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
